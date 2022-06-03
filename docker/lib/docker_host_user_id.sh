c23bdbfecd98504b755bb99e202df3507208d7d6
Chekoté <domhnall.tilliere@gmail.com>
2022-06-03 12:48:21 -0500

    Use DOCKER_HOST_USER_ID of 1000 on any non-Linux OS

--- docker/lib/docker_host_user_id.sh
+++ docker/lib/docker_host_user_id.sh
@@ -2,12 +2,15 @@
 
 # Determines the UID of the user running the Docker containers.
 
-if [ "$(uname)" == 'Darwin' ]; then
-  # We're on Mac OS  X. We're Virtualized using xhyve, and have a UID of 1000.
-  DOCKER_HOST_USER_ID=1000
-else
+if [ "$(uname)" == 'Linux' ]; then
   # We're on Linux. There's no virtualization, so we use our own UID.
   DOCKER_HOST_USER_ID="$(id -u)"
+else
+  # We're not on Linux. The User ID doesn't really matter on non-linux hosts as modern Docker allows any user within
+  # the container to have full read/write access to mounted volumes. But to make it easier for users of this script
+  # to work with the env var, we'll set this to something innocuous so it can still be passed to commands and used in
+  # docker compose config files.
+  DOCKER_HOST_USER_ID=1000
 fi
 
 export DOCKER_HOST_USER_ID
