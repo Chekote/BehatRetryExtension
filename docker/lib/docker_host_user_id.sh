1d573ba7fbcee3ecd1b720ac9ff072ab9170258d
Chekoté <domhnall.tilliere@gmail.com>
2022-06-03 12:45:47 -0500

    fixup! Use delegated volumes for any direct file-system mounts

--- docker/lib/docker_host_user_id.sh
+++ docker/lib/docker_host_user_id.sh
@@ -2,10 +2,10 @@
 
 # Determines the UID of the user running the Docker containers.
 
-if [ `uname` == 'Darwin' ]; then
+if [ "`uname`" == 'Darwin' ]; then
   # We're on Mac OS  X. We're Virtualized using xhyve, and have a UID of 1000.
   export DOCKER_HOST_USER_ID=1000
 else
   # We're on Linux. There's no virtualization, so we use our own UID.
-  export DOCKER_HOST_USER_ID=`id -u`
+  export DOCKER_HOST_USER_ID="`id -u`"
 fi
