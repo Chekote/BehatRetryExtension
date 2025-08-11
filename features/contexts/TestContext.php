c88e577b15998e46da2cdc3bc97ed4d084682f8e
Lorenzo Dal'Aqua <lorenzodalaqua@gmail.com>
2025-08-11 15:53:19 -0300

    Update namespace declaration in TestContext.php

--- features/contexts/TestContext.php
+++ features/contexts/TestContext.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace FriendsOfBehat\TestContext\Context;
+namespace features\contexts;
 
 use Behat\Behat\Context\Context;
 use Symfony\Component\Filesystem\Filesystem;
